458cfcd58fd3dc6236fe2ba125dab28befc91656
nogglee <eunjilee@ieunjiui-MacBookAir.local>
2024-08-13 23:53:19 +0900

    test update

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <h1>심리테스트에 오신 것을 환영합니다!</h1>
+      <h1>심리테스트에 오신 것을 환영하지 않습니다!</h1>
       <p>12가지 질문을 통해 당신의 MBTI 유형을 알아보세요.</p>
       <button onClick={startQuiz}>시작하기</button>
     </div>
