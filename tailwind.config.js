1e6d6d6b1f4ed06164243ac3e21590819ede55de
nogglee <eunjilee@Mac-mini.local>
2024-08-14 13:20:52 +0900

    FIX

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
+// tailwind.config.js
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Tailwind가 적용될 파일들
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        title: ['"Your Title Font"', 'sans-serif'], // 커스텀 폰트 패밀리
-        body: ['"Your Body Font"', 'serif'],
-      },
-      fontSize: {
-        'title-lg': ['32px', '40px'], // 커스텀 폰트 크기
-        'title-md': ['24px', '32px'],
-        'body-lg': ['18px', '28px'],
-        'body-md': ['16px', '24px'],
-      },
-      colors: {
-        primary: '#FF7F00', // 기본 색상
-        secondary: '#153069', // 보조 색상
-      },
+      // 추가적인 테마 설정
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'), // Tailwind Forms 플러그인 추가 예시
+  ],
 };
