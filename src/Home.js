45a4acfddc2c5c749b828c619ecb9bd6c5f1b1f8
nogglee <eunjilee@Mac-mini.local>
2024-08-14 15:35:06 +0900

    mobile test

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ const Home = () => {
   };
 
   return (
-    <div class="bg-gray-200 h-screen flex justify-center items-center">
-      <div class="w-full h-full max-w-5xl bg-white border-black border md:border-none">
-        <div class="w-full h-full max-w-md md:max-w-full bg-gray-400 border-red-500 md:border-none border-4">
+    <div class="h-screen flex justify-center items-center">
+      <div class="w-full h-full max-w-5xl bg-white">
+        <div class="w-full h-full max-w-md md:max-w-full bg-gray-400">
           <div className="home-container">
             <h1>갓물주 운명 테스트</h1>
             <p>당신은 과연 건물을 가질 수 있을까요?</p>
